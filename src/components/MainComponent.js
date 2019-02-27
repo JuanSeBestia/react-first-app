1f22648b8bcee5c4f8fbde423ee4661597f4ae5b
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2019-02-27 18:36:32 -0500

    create forms + thunk + logger

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,8 @@ import HomePage from './pages/HomePage';
 import Contact from './pages/ContactComponent';
 import DishDetail from './menu/DishdetailComponent';
 import About from './pages/AboutPage';
-import { addComment } from '../service/redux/ActionCreators'
+import { addComment, fetchDishes } from '../service/redux/ActionCreators'
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
 
@@ -22,7 +23,9 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
 })
 
 class MainComponent extends Component {
@@ -34,22 +37,30 @@ class MainComponent extends Component {
         console.log("Hellow MainComponent", { state: this.state });
     }
 
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
+
     render() {
         const HomePageElement = () => {
             return (
                 <HomePage
-                    dish={this.props.dishes.find(dish => dish.featured)}
-                    promotion={this.props.promotions.find(promo => promo.featured)}
-                    leader={this.props.leaders.find(leader => leader.featured)}
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
-            )
+            );
         }
 
         const DishWithId = ({ match }) => {
             const dishId = parseInt(match.params.dishId);
             return (
                 <DishDetail
-                    dish={this.props.dishes.find(dish => dish.id === dishId)}
+                    dish={this.props.dishes.dishes.find(dish => dish.id === dishId)}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
                     comments={this.props.comments.filter(comment => comment.dishId === dishId)}
                     addComment={this.props.addComment}
                 />
@@ -68,7 +79,7 @@ class MainComponent extends Component {
                     <Route path="/home" component={HomePageElement} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />} />
-                    <Route exact path='/contactus' component={Contact} />} />
+                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                     <Route exact path='/aboutus' component={AboutPage} />} />
                     <Redirect to="/home" />
                 </Switch>
--- src/service/redux/comments.js
+++ src/service/redux/comments.js
@@ -2,7 +2,6 @@ import { COMMENTS } from '../../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = COMMENTS, action) => {
-    console.log("COMMETNS:ACTION?", action)
     switch (action.type) {
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
